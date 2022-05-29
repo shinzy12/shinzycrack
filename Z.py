7889e8e25e8a4e1ddc28d349a8c640d61248e6e9
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-05-29 14:17:23 +0700

    Update Z.py

--- Z.py
+++ Z.py
@@ -1290,37 +1290,7 @@ class Main:
 		self.loop +=1
 
 
-def Subscraption():
-	key1=open('', 'r').read()
-	clear()
-	print(logo)
-	r1=requests.get("https://pastebin.com/raw/XySKuti7").text
-	if key1 in r1:
-		os.system('clear')
-		print(logo)
-		Main()
-	else:
-		os.system("clear")
-		print(logo)
-		print("\t \033[1;32m First Get Approvel\033[1;37m ")
-		time.sleep(1)
-		os.system("clear")
-		print(logo)
-		print ("")
-		print(" \033[1;32m SHIN Toll Paid You Need Get Approved First\033[1;37m\n")
-		print(" \033[1;32m Note : Paid Tolls \033[1;37m")
-		print ("")
-		print(" Your Key is Not Approved ")
-		print("")
-		print(" Copy And Send Key To Admin")
-		print ("")
-		print (" Your Key : "+ak+ahsan+key1)
-		print ("")
-		name = input(" Your Name : ")
-		print ("")
-		input(" Press Enter To Send Key")
-		time.sleep(3.5)
-		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
-		os.system('am start https://wa.me/+6286740559154?text=' + tks)
-		Subscraption()        
-Subscraption()
+if __name__=="__main__":
+    os.system("clear")
+    mkdir_data_login()
+    menu()
